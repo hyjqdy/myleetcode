79a78801094de191d2860c326db7d8cbd69d9268
hyjqdy <hyjqdy@gmail.com>
2020-03-08 11:44:32 +0800

    Add solution for GuShen

---
+++ src/main/java/leetcode/GuShen.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class GuShen {
+    public static void main(String[] args) {
+        int date;
+        Scanner sc = new Scanner(System.in);
+        date = sc.nextInt();
+
+        int price = 1, priceDownDate = 3, priceDownNum = 1;
+        for (int i = 2; i <= date; i++) {
+            if (priceDownDate == i) {
+                priceDownNum++;
+                priceDownDate = (priceDownNum * priceDownNum + 3 * priceDownNum) / 2 + 1;
+                price--;
+            } else {
+                price++;
+            }
+        }
+        System.out.println(price);
+    }
+}
